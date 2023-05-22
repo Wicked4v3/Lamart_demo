710158dd449647fe66203b73a7857b1db767ec81
Wicked4v3 <37067337+Wicked4v3@users.noreply.github.com>
2023-05-22 06:10:49 +0200

    Add files via upload

--- bash scripts/convert_images.sh
+++ bash scripts/convert_images.sh
@@ -7,21 +7,22 @@ outputLocation=../nettside/img/gallery/
 
 for ((i=1; i<=$numberOfImages; i++))
 do
-    # Special case for image 12
+    # Special case for last image of the initial gallery. It often ends up dynamically placed in such a way 
+    # that takes up the entire screenwidth. Therefore it should be of higher resolution.
     if [ $i -eq 12 ]; then
         echo "Processing image 12..."
-        convert "$originalImageLocation$i$originalImageFormat" -resize 414x -quality 90 "$outputLocation${i}x414.webp"
-        convert "$originalImageLocation$i$originalImageFormat" -resize 855x -quality 90 "$outputLocation${i}x855.webp"
-        convert "$originalImageLocation$i$originalImageFormat" -resize 1280x -quality 90 "$outputLocation${i}x1280.webp"
-        convert "$originalImageLocation$i$originalImageFormat" -resize 1536x -quality 90 "$outputLocation${i}x1536.webp"
-        convert "$originalImageLocation$i$originalImageFormat" -resize 1920x -quality 90 "$outputLocation${i}x1920.webp"
+        convert "$originalImageLocation$i$originalImageFormat" -resize 414x -quality 80 "$outputLocation${i}x414.webp"
+        convert "$originalImageLocation$i$originalImageFormat" -resize 855x -quality 80 "$outputLocation${i}x855.webp"
+        convert "$originalImageLocation$i$originalImageFormat" -resize 1280x -quality 80 "$outputLocation${i}x1280.webp"
+        convert "$originalImageLocation$i$originalImageFormat" -resize 1536x -quality 80 "$outputLocation${i}x1536.webp"
+        convert "$originalImageLocation$i$originalImageFormat" -resize 1920x -quality 80 "$outputLocation${i}x1920.webp"
         echo "OK"
     else
         echo "Processing image $i..."
-        convert "$originalImageLocation$i$originalImageFormat" -resize 414x -quality 80 "$outputLocation${i}x414.webp" #414
-        convert "$originalImageLocation$i$originalImageFormat" -resize 632x -quality 80 "$outputLocation${i}x632.webp" #1280
-        convert "$originalImageLocation$i$originalImageFormat" -resize 760x -quality 80 "$outputLocation${i}x760.webp" #1536
-        convert "$originalImageLocation$i$originalImageFormat" -resize 950x -quality 80 "$outputLocation${i}x950.webp" #1920
+        convert "$originalImageLocation$i$originalImageFormat" -resize 414x -quality 80 "$outputLocation${i}x414.webp"
+        convert "$originalImageLocation$i$originalImageFormat" -resize 632x -quality 80 "$outputLocation${i}x632.webp"
+        convert "$originalImageLocation$i$originalImageFormat" -resize 760x -quality 80 "$outputLocation${i}x760.webp"
+        convert "$originalImageLocation$i$originalImageFormat" -resize 950x -quality 80 "$outputLocation${i}x950.webp"
         echo "OK"
     fi
 done
